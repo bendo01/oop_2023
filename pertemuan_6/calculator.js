4f66a144a9a94f6ff28313266e5b219339eb6f2c
bendo01 <bendo01@gmail.com>
2023-11-16 14:27:07 +0800

    pertemuan 6

---
+++ pertemuan_6/calculator.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+let operand_1 = prompt("masukkan operand 1: ");
+let operator = prompt("masukkan operator: ");
+let operand_2 = prompt("masukkan operand 2: ");
+
+console.info(
+    "hasil dari " + operand_1 + " " + operator + " " + operand_2 + " = ....xx"
+);
